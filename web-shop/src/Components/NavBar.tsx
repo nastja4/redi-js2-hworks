aa9c3bd6600166914765fd2d861a1a5187cf8418
Anastasia Fazius <AnastasiaFazius@gmx.de>
2024-04-26 23:49:02 +0200

    Removes Container in NavBar, adds marginLeft to NavBar.Brand

--- web-shop/src/Components/NavBar.tsx
+++ web-shop/src/Components/NavBar.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../App.css";
@@ -7,23 +6,23 @@ export function NavBar() {
   return (
     <>
       <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
-          <Navbar.Brand as={Link} to="/">
-            Web-Shop.app
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/about">
-              About
-            </Nav.Link>
-            <Nav.Link as={Link} to="/categories">
-              Categories
-            </Nav.Link>
-          </Nav>
-        </Container>
+        {/* <Container> */}
+        {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
+        <Navbar.Brand as={Link} to="/" style={{ marginLeft: "20px" }}>
+          Web-Shop.app
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about">
+            About
+          </Nav.Link>
+          <Nav.Link as={Link} to="/categories">
+            Categories
+          </Nav.Link>
+        </Nav>
+        {/* </Container> */}
       </Navbar>
     </>
   );
